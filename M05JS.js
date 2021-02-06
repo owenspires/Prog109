d187d6714b9d1b7b588c322f96dee473bcab5f42
Owen <omjspires@gmail.com>
2021-02-06 14:05:49 -0800

    test

---
+++ M05JS.js
@@ -0,0 +1,24 @@
+var num = prompt('Put in a number for your multiplication set')
+
+var table = num;             // Unit of table
+var operator = ''; // Type of calculation
+var i = 1;                 // Set counter to 1
+var msg = '<h2>Multiplication</h2>';              // Message
+
+if (operator === 'addition') {
+  // Do addition
+  while (i < 11) {
+    msg += i + ' + ' + table + ' = ' + (i + table) + '<br/>';
+    i++;
+  }
+} else {
+  // Do multiplication
+  while (i < 11) {
+    msg += i + ' x ' + table + ' = ' + (i * table) + '<br/>';
+    i++;
+  }
+}
+
+// Write the message into the page
+var el = document.getElementById('blackboard');
+el.innerHTML = msg;
